b79cec7868dfc17cd13ad9743ba34348c8c298a1
DonJimbo <jaime.rodriguezdeledesma@gmail.com>
2025-08-26 20:02:47 +0200

    Add files via upload

---
+++ Day 2 - Understanding Data Types and How to Manipulate Strings/Data Types/task.py
@@ -0,0 +1,20 @@
+#Subscripting = The method of pulling out a particular element from a string.
+#Number (+) = From 0 to onwards, it selects the number of the string selected
+#Number (-) = From 0 to negative, starts from the end.
+print("Hello"[0])
+
+#String / Concatenation
+print("123"+"456")
+
+#Integer = Whole Number
+print(123 + 456)
+
+#Large Integers
+print(123_456_789)
+
+#Float = Floating Point Number (The decimal point can occur at any point around the number).
+3.14159
+
+# Boolean
+print(True)
+print(False)
